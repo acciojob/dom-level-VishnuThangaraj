2ef819d2746e80a0c5c9feaf53751512d817bee2
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-28 20:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const items = [...document.getElementsByTagName('li')];
+
+for(let index=0; index<items.length; index++){
+	if(items[index].id == 'level') {
+		alert(`The level of the element is: ${index}`);
+	}
+}
